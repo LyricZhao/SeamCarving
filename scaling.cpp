68970c557a4d3e953d6ae3159a13987a19da03f0
Chenggang Zhao <lyricz@yeah.net>
2020-12-26 23:47:37 +0800

    Finish seam finding

--- scaling.cpp
+++ scaling.cpp
@@ -1,25 +1,115 @@
+#include <iostream>
 #include <vector>
 
 #include "scaling.h"
 
-typedef long long cost_t;
+typedef int64_t cost_t;
+typedef int8_t mark_t;
 
 enum Direction {
     Horizontal,
     Vertical
 };
 
-std::vector<int> find_seam(const QImage &image, Direction direction, cost_t* workspace) {
-    int length = direction == Horizontal ? image.width() : image.height();
-    int another = direction == Horizontal ? image.height() : image.width();
-    std::vector<int> seam(length);
-    seam[0] = rand() % another;
-    for (int i = 1; i < length; ++ i) {
-        int k;
-        do {
-            k =  rand() % 3 - 1 + seam[i - 1];
-        } while (k < 0 or k >= another);
-        seam[i] = k;
+std::vector<int> find_seam(const QImage &image, Direction direction, cost_t *dp_space, mark_t *mark_space) {
+    int w = image.width(), h = image.height();
+    std::memset(mark_space, 0, w * h * sizeof(mark_t));
+    auto color_distance = [](const QRgb &color_a, const QRgb &color_b) -> cost_t {
+        int r = qRed(color_a); r -= qRed(color_b); if (r < 0) r = -r;
+        int g = qGreen(color_a); g -= qGreen(color_b); if (g < 0) g = -g;
+        int b = qBlue(color_a); b -= qBlue(color_b); if (b < 0) b = -b;
+        return r + g + b;
+    };
+    for (int y = 0, index = 0; y < h; ++ y) {
+        for (int x = 0; x < w; ++ x, ++ index) {
+            dp_space[index] = 0;
+            int count = 0;
+            if (x > 0) {
+                dp_space[index] += color_distance(image.pixel(x, y), image.pixel(x - 1, y));
+                ++ count;
+            }
+            if (x + 1 < w) {
+                dp_space[index] += color_distance(image.pixel(x, y), image.pixel(x + 1, y));
+                ++ count;
+            }
+            if (y > 0) {
+                dp_space[index] += color_distance(image.pixel(x, y), image.pixel(x, y - 1));
+                ++ count;
+            }
+            if (y + 1 < h) {
+                dp_space[index] += color_distance(image.pixel(x, y), image.pixel(x, y + 1));
+                ++ count;
+            }
+        }
+    }
+
+    auto update = [](cost_t &value, cost_t new_value, mark_t &mark, mark_t new_mark) {
+        if (new_value < value) {
+            value = new_value;
+            mark = new_mark;
+        }
+    };
+    std::vector<int> seam;
+    if (direction == Horizontal) {
+        for (int x = 1; x < w; ++ x) {
+            int index = x;
+            for (int y = 0; y < h; ++ y, index += w) {
+                cost_t dp = 0x3fffffff, energy = dp_space[index];
+                if (y > 0) {
+                    update(dp, energy + dp_space[index - w - 1], mark_space[index], -1);
+                }
+                update(dp, energy + dp_space[index - 1], mark_space[index], 0);
+                if (y + 1 < h) {
+                    update(dp, energy + dp_space[index + w - 1], mark_space[index], 1);
+                }
+                dp_space[index] = dp;
+            }
+        }
+        int y_pos = 0, index = w - 1;
+        cost_t min_value = 0x3fffffff;
+        for (int y = 0; y < h; ++ y, index += w) {
+            if (dp_space[index] < min_value) {
+                y_pos = y;
+                min_value = dp_space[index];
+            }
+        }
+        seam.reserve(w);
+        for (int x = w - 1; x >= 0; -- x) {
+            seam.push_back(y_pos);
+            assert(y_pos >= 0 and y_pos < h);
+            y_pos += mark_space[y_pos * w + x];
+        }
+        std::reverse(seam.begin(), seam.end());
+    } else {
+        for (int y = 1; y < h; ++ y) {
+            int index = y * w;
+            for (int x = 0; x < w; ++ x, ++ index) {
+                cost_t dp = 0x3fffffff, energy = dp_space[index];
+                if (x > 0) {
+                    update(dp, energy + dp_space[index - w - 1], mark_space[index], -1);
+                }
+                update(dp, energy + dp_space[index - w], mark_space[index], 0);
+                if (x + 1 < w) {
+                    update(dp, energy + dp_space[index - w + 1], mark_space[index], 1);
+                }
+                dp_space[index] = dp;
+            }
+        }
+        int x_pos = 0, index = (h - 1) * w;
+        cost_t min_value = 0x3fffffff;
+        for (int x = 0; x < w; ++ x, ++ index) {
+            if (dp_space[index] < min_value) {
+                x_pos = x;
+                min_value = dp_space[index];
+            }
+        }
+        seam.reserve(h);
+        for (int y = h - 1; y >= 0; -- y) {
+            seam.push_back(x_pos);
+            assert(x_pos >= 0 and x_pos < w);
+            x_pos += mark_space[y * w + x_pos];
+        }
+        std::reverse(seam.begin(), seam.end());
     }
     return seam;
 }
@@ -90,11 +180,12 @@ QImage scale(const QImage &image, const QSize &size) {
     QImage scaled = image;
     int max_w = std::max(image.width(), size.width());
     int max_h = std::max(image.height(), size.height());
-    auto *workspace = static_cast<cost_t*>(std::malloc(max_w * max_h * sizeof(cost_t)));
+    auto *dp_space = static_cast<cost_t*>(std::malloc(max_w * max_h * sizeof(cost_t)));
+    auto *mark_space = static_cast<mark_t*>(std::malloc(max_w * max_h * sizeof(mark_t)));
 
     // Scale by height
     while (scaled.height() != size.height()) {
-        auto seam = find_seam(scaled, Horizontal, workspace);
+        auto seam = find_seam(scaled, Horizontal, dp_space, mark_space);
         scaled = scaled.height() < size.height() ?
                  add_seam(scaled, seam, Horizontal):
                  delete_seam(scaled, seam, Horizontal);
@@ -102,13 +193,14 @@ QImage scale(const QImage &image, const QSize &size) {
 
     // Scale by width
     while (scaled.width() != size.width()) {
-        auto seam = find_seam(scaled, Vertical, workspace);
+        auto seam = find_seam(scaled, Vertical, dp_space, mark_space);
         scaled = scaled.width() < size.width() ?
                  add_seam(scaled, seam, Vertical):
                  delete_seam(scaled, seam, Vertical);
     }
 
     // Free and return
-    std::free(workspace);
+    std::free(dp_space);
+    std::free(mark_space);
     return scaled;
 }
